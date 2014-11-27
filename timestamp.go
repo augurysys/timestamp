c0376352024cc00c8c90a95ec494ad02ac7ce2d3
Gal Ben-Haim <gbenhaim@augury.com>
2014-11-28 00:49:07 +0200

    first commit

---
+++ timestamp.go
@@ -0,0 +1,136 @@
+// Package timestamp is used for marshaling/unmarshaling UNIX timestamps
+// to/from JSON, GOB and BSON by implementing the appropriate interfaces for
+// encoding/json, encoding/gob and labix.org/v2/mgo respectively.
+//
+// by gbehaim@augury.com
+package timestamp
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"labix.org/v2/mgo/bson"
+	"strconv"
+	"time"
+)
+
+// Timestamp is a named alias for time.Time,
+// it represents a UNIX timestamp
+// and provides functions for marshaling and unmarshaling both to/from JSON and
+// to/from BSON
+type Timestamp time.Time
+
+// MarshalJSON defines how encoding/json marshals the object to JSON,
+// the result is a string of the UNIX timestamp
+func (t *Timestamp) MarshalJSON() ([]byte, error) {
+	ts := t.Time().Unix()
+	stamp := fmt.Sprint(ts)
+
+	return []byte(stamp), nil
+}
+
+// UnmarshalJSON defines how encoding/json unmarshals the object from JSON,
+// a UNIX timestamp string is converted to int which is used for the Timestamp
+// object value
+func (t *Timestamp) UnmarshalJSON(b []byte) error {
+	ts, err := strconv.Atoi(string(b))
+	if err != nil {
+		return err
+	}
+
+	*t = Timestamp(time.Unix(int64(ts), 0))
+
+	return nil
+}
+
+// GetBSON defines how labix.org/v2/mgo marshals the object to BSON,
+// the result is a time.Time object which is then handled by mgo
+func (t Timestamp) GetBSON() (interface{}, error) {
+	if t.Time().IsZero() {
+		return nil, nil
+	}
+
+	return t.Time(), nil
+}
+
+// SetBSON defines how labix.org/v2/mgo unmarshals the object from BSON,
+// the raw BSON data is unmarshaled to a time.Time object which is used for the
+// Timestamp object value
+func (t *Timestamp) SetBSON(raw bson.Raw) error {
+	var tm time.Time
+
+	if err := raw.Unmarshal(&tm); err != nil {
+		return err
+	}
+
+	*t = Timestamp(tm)
+
+	return nil
+}
+
+// String returns the string representation of the Timestamp object,
+// it is equal to the time.Time string representation of the Timestamp object
+// value
+func (t Timestamp) String() string {
+	return t.Time().String()
+}
+
+// Time returns a time.Time object with the same time value as the Timestamp
+// object
+func (t *Timestamp) Time() time.Time {
+	return time.Time(*t)
+}
+
+// Now returns a pointer to a Timestamp object with the current time,
+// it is equal to creating a Timestamp object from time.Now()
+func Now() *Timestamp {
+	t := Timestamp(time.Now())
+	return &t
+}
+
+// Unix calls the Unix() method of a time.Time object with the same time values
+// as the timestamp object
+func (t *Timestamp) Unix() int64 {
+	return t.Time().Unix()
+}
+
+// Time returns a pointer to a Timestamp object which is created
+// from a time.Time object
+func Time(t time.Time) *Timestamp {
+	ts := Timestamp(t)
+	return &ts
+}
+
+// Unix returns a pointer to a Timestamp object which is created from
+// a UNIX timestamp
+func Unix(sec, nsec int64) *Timestamp {
+	t := time.Unix(sec, nsec)
+	return Time(t)
+}
+
+// GobEncode returns a byte slice representing the encoding of the Timestamp
+// object, it implements the GobEncoder interface
+func (t *Timestamp) GobEncode() ([]byte, error) {
+	var b bytes.Buffer
+	enc := gob.NewEncoder(&b)
+
+	err := enc.Encode(t.Time())
+
+	return b.Bytes(), err
+}
+
+// GobDecode decodes a Timestamp object from a byte slice
+// and overwrites the receiver,
+// it implements the GobDecoder interface
+func (t *Timestamp) GobDecode(b []byte) error {
+	r := bytes.NewReader(b)
+	var tm time.Time
+
+	dec := gob.NewDecoder(r)
+	if err := dec.Decode(&tm); err != nil {
+		return err
+	}
+
+	*t = Timestamp(tm)
+	return nil
+}
---
+++ timestamp_test.go
@@ -0,0 +1,138 @@
+package timestamp
+
+import (
+	"bytes"
+	"encoding/gob"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestMarshalJSON(t *testing.T) {
+	tm := time.Unix(3000, 0)
+	ts := Timestamp(tm)
+
+	b, err := ts.MarshalJSON()
+	if err != nil {
+		t.Error(err)
+	}
+
+	temp, err := strconv.Atoi(string(b))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if temp != 3000 {
+		t.Fail()
+	}
+}
+
+func TestUnmarshalJSON(t *testing.T) {
+	tm := time.Unix(3000, 0)
+	ts := Timestamp(tm)
+
+	b, err := ts.MarshalJSON()
+	if err != nil {
+		t.Error(err)
+	}
+
+	var temp Timestamp
+
+	if err := temp.UnmarshalJSON(b); err != nil {
+		t.Error(err)
+	}
+
+	if temp != ts {
+		t.Fail()
+	}
+}
+
+func TestString(t *testing.T) {
+	tm := time.Unix(3000, 0)
+	ts := Timestamp(tm)
+
+	if tm.String() != ts.String() {
+		t.Fail()
+	}
+}
+
+func TestGetBSON(t *testing.T) {
+	tm := time.Unix(3000, 0)
+	ts := Timestamp(tm)
+
+	result, err := ts.GetBSON()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if result != tm {
+		t.Fail()
+	}
+}
+
+func TestTime(t *testing.T) {
+	tm := time.Unix(3000, 0)
+	ts := Timestamp(tm)
+
+	if ts.Time() != tm {
+		t.Fail()
+	}
+}
+
+func TestNow(t *testing.T) {
+	tm := time.Now()
+	ts := Now()
+
+	if tm.Unix() != ts.Time().Unix() {
+		t.Fail()
+	}
+}
+
+func TestUnix(t *testing.T) {
+	tm := time.Unix(3000, 0)
+	ts := Timestamp(tm)
+
+	if ts.Unix() != tm.Unix() {
+		t.Fail()
+	}
+}
+
+func TestFromTime(t *testing.T) {
+	tm := time.Unix(3000, 0)
+	ts := Time(tm)
+
+	if ts.Unix() != tm.Unix() {
+		t.Fail()
+	}
+}
+
+func TestFromUnix(t *testing.T) {
+	tm := time.Unix(3000, 0)
+	ts := Unix(3000, 0)
+
+	if ts.Time() != tm {
+		t.Fail()
+	}
+}
+
+func TestGobEncodeDecode(t *testing.T) {
+	ts := Now()
+
+	var b bytes.Buffer
+	enc := gob.NewEncoder(&b)
+	if err := enc.Encode(ts); err != nil {
+		t.Fatal(err)
+	}
+
+	var n Timestamp
+	r := bytes.NewBuffer(b.Bytes())
+
+	dec := gob.NewDecoder(r)
+	if err := dec.Decode(&n); err != nil {
+		t.Fatal(err)
+	}
+
+	if ts.Time() != n.Time() {
+		t.Fail()
+	}
+}
